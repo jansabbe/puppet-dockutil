b0e24d443458a3ea3cb2912c54435a76f2592a9f
Jan Sabbe <jan.sabbe@gmail.com>
2013-05-07 20:59:45 +0200

    added sleep, dock acts weird

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,7 +1,7 @@
 class dockutil::base {
 
 	exec {"refresh dock": 
-		command =>  "killall Dock && ${boxen::config::cachedir}/dockutil/scripts/dockutil --list && killall Dock",
+		command =>  "sleep 10 && killall Dock && ${boxen::config::cachedir}/dockutil/scripts/dockutil --list && killall Dock",
 		refreshonly => true,
 	}
 
