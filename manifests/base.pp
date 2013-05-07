ef7237c0e04087533a2a9aea0ce45754d7bebeb4
Jan Sabbe <jan.sabbe@gmail.com>
2013-05-07 21:37:00 +0200

    fix reloading dock

--- manifests/base.pp
+++ manifests/base.pp
@@ -3,6 +3,10 @@ class dockutil::base {
 	exec {"refresh dock": 
 		command =>  "sleep 10 && killall Dock && ${boxen::config::cachedir}/dockutil/scripts/dockutil --list && killall Dock",
 		refreshonly => true,
+		notify => Exec["refresh dock again"]
+	}	
+	exec {"refresh dock again": 
+		command =>  "sleep 10 && killall Dock && ${boxen::config::cachedir}/dockutil/scripts/dockutil --list && killall Dock",
+		refreshonly => true,
 	}
-
 } 
